01d206cb1a13443536aaa9292dac2e36753846c5
Samuel Mota <smagnun@gmail.com>
2019-07-14 18:39:13 +0200

    pagination from search terms

--- classes/Search.php
+++ classes/Search.php
@@ -11,22 +11,25 @@ class Search {
 
   // }
 
-	public static function searchTerms($termsToSearch, $startRow = 0, $totalRows = 10) {
+	public static function searchTerms($termsToSearch, $page = 1, $totalRowsPerPage) {
+    if($page > 1):
+      $startRow = ($page - 1) * $totalRowsPerPage;
+    else:
+      $startRow = 0;
+    endif;
 
     $query = "SELECT livro_nome,
                       cap, 
                       ver, 
                       texto,
                       livro_slug,
-                      MATCH (texto) 
-                      AGAINST (:terms) 
+                      MATCH (texto) AGAINST (:terms) 
                       AS SCORE 
-                      FROM bibliakja AS biblia
-                      LEFT JOIN biblia_livros AS bl
-                      ON bl.cod_livro = biblia.cod_livro
-                      WHERE MATCH (texto) 
-                      AGAINST (:terms) > 0
-                      LIMIT $startRow,$totalRows";
+    FROM bibliakja AS biblia
+    LEFT JOIN biblia_livros AS bl
+    ON bl.cod_livro = biblia.cod_livro
+    WHERE MATCH (texto) AGAINST (:terms) > 0
+    LIMIT $startRow,$totalRowsPerPage";
 
 		$connection = Connect::getConnection();
 		$statement = $connection->prepare($query);
@@ -36,7 +39,9 @@ class Search {
 		return $statement->fetchAll();
   }
   
-  public static function searchTermsByBooks($termsToSearch, $book, $startRow = 0, $totalRows = 10) { //$book = livro_slug
+  
+  public static function searchTermsByBook($termsToSearch, $book, $startRow = 0, $totalRowsPerPage) { 
+    //$book = livro_slug
 
     $query = "SELECT livro_nome,
                       cap, 
@@ -45,25 +50,27 @@ class Search {
                       livro_slug,
                       MATCH (texto) AGAINST (:terms) 
                       AS SCORE 
-                      FROM bibliakja AS biblia
-                      LEFT JOIN biblia_livros AS bl
-                      ON bl.cod_livro = biblia.cod_livro
-                      WHERE MATCH (texto) 
-                      AGAINST (:terms) > 0
-                      AND livro_slug = $book
-                      LIMIT $startRow,$totalRows";
+    FROM bibliakja AS biblia
+    LEFT JOIN biblia_livros AS bl
+    ON bl.cod_livro = biblia.cod_livro
+    WHERE MATCH (texto) AGAINST (:terms) > 0
+    AND livro_slug = :book
+    LIMIT $startRow,$totalRowsPerPage";
 
 		$connection = Connect::getConnection();
 		$statement = $connection->prepare($query);
 
 		$statement->bindValue(':terms', $termsToSearch);
+		$statement->bindValue(':book', $book);
 		$statement->execute();
 		return $statement->fetchAll();
-	}
+  }
+  
 
-  public static function totalRowsByBooks ($termsToSearch) {
+  public static function totalRowsByBooks($termsToSearch) {
 
-    $query = "SELECT livro_nome, COUNT(*) AS TOTAL
+    $query = "SELECT  livro_nome, 
+                      COUNT(*) AS TOTAL
     FROM bibliakja AS biblia
     LEFT JOIN biblia_livros AS bl
     ON bl.cod_livro = biblia.cod_livro
@@ -78,7 +85,8 @@ class Search {
     return $statement->fetchAll();
   }
 
-  public static function totalRows ($termsToSearch) {
+
+  public static function totalRows($termsToSearch) {
 
     $query = "SELECT COUNT(cod_livro) AS TOTAL
     FROM bibliakja
@@ -91,5 +99,68 @@ class Search {
     $statement->execute();
     return $statement->fetch();
   }
+
+
+  public function paginationLinks($totalPages, $pagesStart, $pagesMid, $pagesEnd, $pageSelected, $terms) {
+    $allLinksTogether = "";
+    $linkCreated = "";
+
+    // CONSTRUCT PAGINATION AND 
+    // HIGHLIGHT THE SELECTED ONE
+    foreach ($pagesStart as $pages):
+      $linkCreated = $this->paginationLinksCreator($terms, $pages, $pageSelected);
+
+      $allLinksTogether = $allLinksTogether . $linkCreated;
+
+      //$allLinksTogether = $allLinksTogether . "<a href='/pesquisa.php?termos=$terms&pagina=$pages' class='pagination--number'>$pages</a>";
+    endforeach;
+
+    if (!empty($pagesMid)):
+      $allLinksTogether = $allLinksTogether . " ... ";
+
+      foreach ($pagesMid as $pages):
+        $linkCreated = $this->paginationLinksCreator($terms, $pages, $pageSelected);
+
+        $allLinksTogether = $allLinksTogether . $linkCreated;
+
+        //$allLinksTogether = $allLinksTogether . "<a href='/pesquisa.php?termos=$terms&pagina=$pages' class='pagination--number'>$pages</a>";
+      endforeach;
+
+      //echo implode(" ", $pagesStart) . " ... " . implode(" ", $pagesMid) . " ... " . implode(" ", $pagesEnd);
+    endif;
+    
+    if (!empty($pagesEnd)):
+      $allLinksTogether = $allLinksTogether . " ... ";
+
+      foreach ($pagesEnd as $pages):
+        $linkCreated = $this->paginationLinksCreator($terms, $pages, $pageSelected);
+
+        $allLinksTogether = $allLinksTogether . $linkCreated;
+        
+        //$allLinksTogether = $allLinksTogether . "<a href='/pesquisa.php?termos=$terms&pagina=$pages' class='pagination--number'>$pages</a>";
+      endforeach;
+      //echo implode(" ", $pagesStart) . " ... " . implode(" ", $pagesEnd);
+    endif;
+    
+    //echo "<a href='/pesquisa.php?termos=$terms&pagina=$i' class='pagination--number'>$i</a>";
+
+
+    return $allLinksTogether;
+  }
+
+  private function paginationLinksCreator($terms, $pages, $pageSelected) {
+    // if ($pageSelected != $pages):
+    //   $linkCreated = paginationLinksCreator($terms, $pages);
+    // else:
+    //   $linkCreated = paginationLinksCreator($terms, $pages, true);
+    // endif;
+
+    return ($pageSelected != $pages) ? 
+      // common link
+      "<a href='/pesquisa.php?termos=$terms&pagina=$pages' class='pagination--number'>$pages</a>": 
+      // selected link
+      "<a href='/pesquisa.php?termos=$terms&pagina=$pages' class='pagination--number pagination--number__selected'>$pages</a>";
+  }
 }
+
 ?>
--- pesquisa.php
+++ pesquisa.php
@@ -14,6 +14,12 @@
     //$button = $_GET [ 'submit' ]; 
     $terms = htmlspecialchars($_GET['termos'], ENT_QUOTES, 'UTF-8'); //transform characters in html notation
 
+    if(isset($_GET["pagina"]) && is_numeric($_GET["pagina"]) && $_GET["pagina"] >= 1): //is_int retorna false :(
+      $pageSelected = $_GET["pagina"];
+    else:
+      $pageSelected = 1;
+    endif;
+
 	} catch (Exception $e) {
 		echo ('MENSAGEM:' . $e->getMessage());
 	}
@@ -28,12 +34,14 @@
     <section class="search-terms">
 
     <?php 
+        //echo $pageSelected;
+      $rowsPerPage = 20;
 
       if (strlen($terms) <= 1) :
-        echo "<p>Termo muito curto, palavra deve conter pelo menos 2 caracteres</p>"; 
+        echo "<p>Termo muito curto, palavra deve conter pelo menos 2 caracteres</p>";
 
       else :
-        $search = Search::searchTerms($terms);
+        $search = Search::searchTerms($terms, $pageSelected, $rowsPerPage);
         //print_r($search);
         //var_dump($search);
 
@@ -64,12 +72,7 @@
 
             </p>
 
-        <?php endforeach;
-
-          //$totalRowsByTestament = ;
-
-          $totalRowsByBooks = Search::totalRowsByBooks($terms);
-        ?>
+        <?php endforeach; ?>
 
 
     <aside class="aside-nav js-aside-modal">
@@ -78,12 +81,17 @@
 
 			<div class="aside-nav__bible--search">
 				
-					<h3 class="aside-nav__bible--intro">Termos encontrados por livros</h3>
+					<h3 class="aside-nav__bible--intro">Termos encontrados por livro</h3>
 
             <!-- ROWS BY BOOKS -->
           <table>
 
-					<?php foreach ($totalRowsByBooks as $row): 
+          <?php 
+          //$totalRowsByTestament = ;
+
+          $totalRowsByBooks = Search::totalRowsByBooks($terms);
+
+          foreach ($totalRowsByBooks as $row): 
 					?>
 						
               <tr>
@@ -100,82 +108,112 @@
 			</div>
 		</aside>
 
-    <!-- PAGINATION -->
-    <?php 
+    <?php // ********** PAGINATION ***********
       $totalRows = Search::totalRows($terms);
-      //$totalPages = ceil($totalRows[0]/5);
-      $totalPages = 2; //PARA TESTE******
-      if(isset($_GET["pagina"])):
-        $pageSelected = $_GET["pagina"];
-      else:
-        $pageSelected = 1;
-      endif;
+      
+      $totalPages = ceil($totalRows[0]/$rowsPerPage);
+      // FOR TEST ******
+      //$totalPages = 10; 
+      // ***************
+
     ?>
 
-    <div>
+    <div class="pagination">
+
       <?php 
-  //- ate 10
-  //-- 1-10 paginas = 1 2 3 4 5 6 7 8 9 10
-  //- acima de 10
-  //-- 4 ... 4 = <<*1* 2 3 4 ... 8 9 10 11>>
-  //-- 5 ... 4 = <<1 2 3 *4* 5 ... 8 9 10 11>>
-  //-- 2...3...2 = <<1 2 ... 4 *5* 6 ... 10 11>>
-  //-- 4 ... 5 = <<1 2 3 4 ... 7 *8* 9 10 11>>
-  //-- 4 ... 4 = <<1 2 3 4 ... 8 *9* 10 11>>
-    if ($totalPages <= 10):
+    $pagesStart = array();
+    $pagesMid = array();
+    $pagesEnd = array();
+    //$pagesAll = array();
+    $numPagesEachSide = 4;
+
+    //- up to 10 pages
+    //-- 1-10 pages = 1 2 3 4 5 6 7 8 9 10
+    if ($totalPages <= 10): 
       for ($i = 1; $i <= $totalPages; $i++): 
-        echo $i . " ";
-      endfor;
-    else:
-      $pagesStart = array();
-      $pagesMid = array();
-      $pagesEnd = array();
-      $pagesAll = array();
-
-      for ($i = 1; $i <= 4; $i++): 
         array_push($pagesStart, $i);
-        array_unshift($pagesEnd, $totalPages--);
+        
+        //echo $i . " ";
       endfor;
+    else: 
+      // more than 10 pages
+      //-- 4 ... 4 = <<*1* 2 3 4 ... 8 9 10 11>>
+      //-- 5 ... 4 = <<1 2 3 *4* 5 ... 8 9 10 11>>
+      //-- 2...3...2 = <<1 2 ... 4 *5* 6 ... 10 11>>
+      //-- 4 ... 5 = <<1 2 3 4 ... 7 *8* 9 10 11>>
+      //-- 4 ... 4 = <<1 2 3 4 ... 8 *9* 10 11>>
 
-      // for ($i = $pageSelected; $i <= 3; $i++): 
+      //**************************************
+      // NEED IMPROVEMENT ********************
+      //**************************************
 
-      // endfor;
+      if($pageSelected <= $numPagesEachSide):
+        for ($i = 1; $i <= $numPagesEachSide; $i++):
+          array_unshift($pagesEnd, $totalPages--);
+        endfor;
 
+        if($pageSelected < $numPagesEachSide):
+          for ($i = 1; $i <= $numPagesEachSide; $i++):
+            array_push($pagesStart, $i);
+          endfor;
 
-      //CRIAR LINKS COM FOREACH OU FOR
-      echo implode(" ", $pagesStart) . " ... " . implode(" ", $pagesEnd);
-    endif;
+        elseif ($pageSelected == $numPagesEachSide):
+          for ($i = 1; $i <= ($numPagesEachSide + 1); $i++):
+            array_push($pagesStart, $i);
+          endfor;
+        endif;
+
+      elseif($pageSelected >= ($totalPages - $numPagesEachSide + 1)):
+        for ($i = 1; $i <= $numPagesEachSide; $i++):
+          array_push($pagesStart, $i);
+        endfor;
+
+        if($pageSelected > ($totalPages - $numPagesEachSide + 1)):
+          for ($i = 1; $i <= $numPagesEachSide; $i++):
+            array_unshift($pagesEnd, $totalPages--);
+          endfor;
+
+        elseif ($pageSelected == ($totalPages - $numPagesEachSide + 1)):
+          for ($i = 1; $i <= ($numPagesEachSide + 1); $i++):
+            array_unshift($pagesEnd, $totalPages--);
+          endfor;
+        endif;
+      
+      else:
+        
+        for ($i = 1; $i <= 2; $i++): // 2 pg each side
+          array_push($pagesStart, $i);
+          array_unshift($pagesEnd, $totalPages--);
+        endfor;
+
+        $count = $pageSelected - 1;
+        for ($i = 1; $i <= 3; $i++): // 3 pg mid
+          array_push($pagesMid, $count++);
+        endfor;
+
+      endif;
+
+    endif; //if ($totalPages <= 10)
+
+      // CONSTRUCT PAGINATION AND 
+      // HIGHLIGHT THE SELECTED ONE
+
+      $pagination = new Search();
+      $paginationLinks = $pagination->paginationLinks($totalPages, $pagesStart, $pagesMid, $pagesEnd, $pageSelected, $terms);
+
+      echo $paginationLinks;
+
+      //echo Search::paginationLinks($totalPages, $pagesStart, $pagesMid, $pagesEnd, $pageSelected, $terms);
       ?>
     </div>
 
-
-        <?php endif;
-
-      endif;
-
-/*     
-- PAGINACAO
-- MOSTRAR QUANTOS VERSICULOS ENCONTRADOS 
-- INCLUIR CAIXAS DE SELECAO PARA QUANTIDADE DE ITENS MOSTRADOS
-********************
-
-
-        SELECT livro_nome, cap, ver, texto, livro_slug, 
-MATCH (texto) AGAINST ("declarou então jesus") AS SCORE 
-FROM bibliakja AS biblia
-LEFT JOIN biblia_livros AS bl
-ON bl.cod_livro = biblia.cod_livro
-WHERE MATCH (texto) AGAINST ("declarou então jesus") > 0
-LIMIT 0,10;
-
-SELECT COUNT(*), 
-MATCH (texto) AGAINST ("declarou então jesus") AS SCORE 
-FROM bibliakja
-WHERE MATCH (texto) AGAINST ("declarou então jesus") > 0; */
-        
-
-
-        
+    <?php endif; //(count($search) == 0)
+      endif; //(strlen($terms) <= 1)
+        /*     
+        - PAGINACAO
+        - MOSTRAR QUANTOS VERSICULOS ENCONTRADOS 
+        - INCLUIR CAIXAS DE SELECAO PARA QUANTIDADE DE ITENS MOSTRADOS
+        ********************/
     ?>
 		
     </section>
@@ -183,4 +221,4 @@ WHERE MATCH (texto) AGAINST ("declarou então jesus") > 0; */
   </section>
 
 
-  <?php require_once 'body__footer.php' ?>
+  <?php require_once 'body__footer.php'; ?>
